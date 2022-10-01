dd8d1f984336089468817caac5418aef58c195bc
Marcos Acosta <macosta1625@gmail.com>
2022-09-30 22:32:49 -0700

    Add username page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,28 @@
+import { useState } from "react";
 import Trainer from "../Trainer/Trainer";
+import UsernamePage from "../UsernamePage/UsernamePage";
 
 export default function App() {
-  return <Trainer />;
+  const [username, setUsername] = useState("");
+  const [usernamePageShouldDisappear, setUsernamePageShouldDisappear] =
+    useState(false);
+  const [finishedDisappearing, setFinishedDisappearing] = useState(false);
+
+  function triggerSetUsername(newUsername) {
+    setUsername(newUsername);
+    setUsernamePageShouldDisappear(true);
+    setTimeout(() => setFinishedDisappearing(true), 250);
+  }
+
+  return (
+    <>
+      {username && <Trainer />}
+      {!finishedDisappearing && (
+        <UsernamePage
+          setUsername={triggerSetUsername}
+          disappear={usernamePageShouldDisappear}
+        />
+      )}
+    </>
+  );
 }
---
+++ src/Components/UsernamePage/UsernamePage.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import {
+  combineClassNames,
+  formatRgbValues,
+  generateRandomRgbValues,
+} from "../../util";
+import styles from "./UsernamePage.module.css";
+
+export default function UsernamePage(props) {
+  const [currentUsername, setCurrentUsername] = useState("");
+  const [randomColor, setRandomColor] = useState([0, 0, 0]);
+
+  function onKeyDown({ key }) {
+    if (key === "Enter" && currentUsername) {
+      props.setUsername(currentUsername);
+    }
+  }
+
+  useEffect(() => {
+    const colorInterval = setInterval(
+      () => setRandomColor(generateRandomRgbValues()),
+      2000
+    );
+    return () => clearInterval(colorInterval);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  });
+
+  return (
+    <div
+      className={combineClassNames(
+        styles.fullWidthContainer,
+        props.disappear && styles.invisible
+      )}
+    >
+      <div className={styles.usernameInputContainer}>
+        <div
+          className={styles.gameTitle}
+          style={{ color: formatRgbValues(...randomColor) }}
+        >
+          rbog
+        </div>
+        <input
+          className={styles.usernameInput}
+          maxLength="10"
+          placeholder="screen name"
+          value={currentUsername}
+          onChange={(e) => setCurrentUsername(e.target.value)}
+          autoFocus={true}
+          style={{
+            width: currentUsername
+              ? `calc(${currentUsername.length}ch + 40px)`
+              : "800px",
+          }}
+        />
+        {currentUsername && (
+          <button
+            className={combineClassNames(
+              styles.enterPrompt,
+              currentUsername && styles.visible
+            )}
+            onClick={() => props.setUsername(currentUsername)}
+          >
+            ↩
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
